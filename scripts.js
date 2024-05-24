a0a0419aa9a0fc9891565e04c93c398c844eb4d7
Lesego Shange <makgeledisal@gmail.com>
2024-05-24 12:17:53 +0200

    Exercise 1

--- scripts.js
+++ scripts.js
@@ -1,15 +1,42 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+// Exercise 1: ForEach Basics
+console.log("Exercise 1: ForEach Basics");
+// Log each name
+names.forEach((name) => console.log(name));
+
+// Log each province
+provinces.forEach((province) => console.log(province));
+
+// Log each name with matching province
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`);
+});
